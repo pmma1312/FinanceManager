42ba6b6acfc4690720f7e303c1c38f64a1f40683
Pm Ma <45858074+pmma1312@users.noreply.github.com>
2020-11-25 11:11:31 +0100

    Update BookingRepository.cs

--- Infrastructure/Repository/BookingRepository.cs
+++ Infrastructure/Repository/BookingRepository.cs
@@ -42,12 +42,12 @@ namespace FinanceManager.Infrastructure.Repository
 
         public async Task<List<Booking>> GetBookingsForMonth(DateTime date, long userId)
         {
-            var lastDayOfMonth = new DateTime(date.Year, date.Month, 25);
-            var firstDayOfMonth = lastDayOfMonth.AddMonths(-1).AddDays(-1);
+            var lastDayOfMonth = new DateTime(date.Year, date.Month, 24);
+            var firstDayOfMonth = lastDayOfMonth.AddMonths(-1).AddDays(1);
 
             return await _context.Bookings
-                .Where(booking => booking.BookingDate > firstDayOfMonth 
-                    && booking.BookingDate < lastDayOfMonth 
+                .Where(booking => booking.BookingDate >= firstDayOfMonth 
+                    && booking.BookingDate <= lastDayOfMonth 
                     && booking.BookingUser.UserId == userId)
                     .Include(booking => booking.BookingCategory)
                     .AsNoTracking()
